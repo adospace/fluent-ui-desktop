c5f253ae7a7810eb270080a4c245adb4bfaa45c2
Adolfo Marinucci <adospace@msn.com>
2021-11-04 00:53:59 +0100

    Initial commit

---
+++ src/FluentUI.Desktop/FluentUI.Desktop.DemoApp/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FluentUI.Desktop.DemoApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var selectedItem = e.NewValue as TreeViewItem;
+            if (selectedItem != null && selectedItem.Tag is string pageTypeName)
+            {
+                pageContainer.Content = System.Reflection.Assembly.GetExecutingAssembly().CreateInstance($"FluentUI.Desktop.DemoApp.Pages.{pageTypeName}");
+            }
+        }
+    }
+}
---
+++ src/FluentUI.Desktop/FluentUI.Desktop/Class1.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace FluentUI.Desktop
+{
+    public class Class1
+    {
+    }
+}
