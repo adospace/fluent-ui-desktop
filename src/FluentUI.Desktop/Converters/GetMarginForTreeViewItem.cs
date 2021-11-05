7b6a4fca81787b52a3458f783859bc800d965195
Adolfo Marinucci <adospace@msn.com>
2021-11-05 20:46:42 +0100

    TreeView

---
+++ src/FluentUI.Desktop/Converters/GetMarginForTreeViewItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FluentUI.Desktop.Converters
+{
+    public class GetNegativeMarginForTreeViewItem : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var treeViewItem = (TreeViewItem)value;            
+            var treeViewItemLevel = GetTreeViewItemLevel(treeViewItem);
+
+            return new Thickness(-12.0 * treeViewItemLevel, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+        
+        private static int GetTreeViewItemLevel(TreeViewItem item)
+        {
+            return (ItemsControl.ItemsControlFromItemContainer(item) is TreeViewItem parent) ? GetTreeViewItemLevel(parent) + 1 : 0;
+        }
+    }
+
+    public class GetPositiveMarginForTreeViewItem : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var treeViewItem = (TreeViewItem)value;
+            var treeViewItemLevel = GetTreeViewItemLevel(treeViewItem);
+
+            return new Thickness(2.0 + 12.0 * treeViewItemLevel, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static int GetTreeViewItemLevel(TreeViewItem item)
+        {
+            return (ItemsControl.ItemsControlFromItemContainer(item) is TreeViewItem parent) ? GetTreeViewItemLevel(parent) + 1 : 0;
+        }
+    }
+}
---
+++ src/FluentUI.Desktop/TreeViewHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FluentUI.Desktop
+{
+    public class TreeViewHelper
+    {
+        #region ShowCheckBoxes Property
+        public static readonly DependencyProperty ShowCheckBoxesProperty =
+            DependencyProperty.RegisterAttached("ShowCheckBoxes", typeof(bool), typeof(TreeViewHelper));
+
+        public static void SetShowCheckBoxes(UIElement element, bool value)
+        {
+            element.SetValue(ShowCheckBoxesProperty, value);
+        }
+        public static bool GetShowCheckBoxes(UIElement element)
+        {
+            return (bool)element.GetValue(ShowCheckBoxesProperty);
+        }
+        #endregion
+
+        #region IsItemChecked Property
+
+        public static readonly DependencyProperty IsItemCheckedProperty =
+            DependencyProperty.RegisterAttached("IsItemChecked", typeof(bool), typeof(TreeViewHelper));
+
+        public static void SetIsItemChecked(UIElement element, bool value)
+        {
+            element.SetValue(IsItemCheckedProperty, value);
+        }
+        public static bool GetIsItemChecked(UIElement element)
+        {
+            return (bool)element.GetValue(IsItemCheckedProperty);
+        }
+
+        #endregion
+    }
+}
