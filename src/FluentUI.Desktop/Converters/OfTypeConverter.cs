761134bfeb790e41691f38c0b4c9ef1ba7fb4285
Adolfo Marinucci <adospace@msn.com>
2021-11-13 15:25:47 +0100

    Completing ComboBox

---
+++ src/FluentUI.Desktop/Converters/OfTypeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace FluentUI.Desktop.Converters
+{
+    public class OfTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var typeToCheck = parameter as Type;
+            if (typeToCheck == null)
+                return false;
+            if (value?.GetType() == typeToCheck)
+                return true;
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
